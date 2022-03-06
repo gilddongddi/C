e8b0df1075b2e7be14af127b18b4fc6d5ff75711
CM.Bae <gilddongii@gmail.com>
2022-03-06 22:49:25 +0900

    add 009~011

---
+++ 011/011.c
@@ -0,0 +1,18 @@
+//문자형 변수 이해하기(char)
+
+#include<stdio.h>
+
+main()
+{
+	char ch;
+	unsigned char j;
+	char k;
+
+	ch = 200;
+	j = 200;
+	k = 'a';
+
+	printf("문자형 변수 ch의 값을 %d\n", ch);
+	printf("문자형 변수 j의 값은 %d\n", j);
+	printf("문자형 변수 k의 값은 %d\n", k);
+}
