16aaa4aaf20921b82137666b110395228ee67c0e
CMBae <gilddongii@gmail.com>
2022-04-13 01:31:37 +0900

    xcode_test

---
+++ c_class/c_lesson06_birthday.c/c_lesson06_birthday.c/main.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+void Birthday(){
+    printf("생일축하합니다\n");
+    printf("생일축하해요\n");
+}
+
+int main(void)
+{
+    int a=5;
+    int b=5;
+    int result = a+b;
+    printf("%d\n", result);
+    Birthday();
+}
