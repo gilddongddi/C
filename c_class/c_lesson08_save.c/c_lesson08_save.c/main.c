b38ac1d13042fc1353a51a3a49d17645944cc8fd
CMBae <gilddongii@gmail.com>
2022-05-03 23:59:36 +0900

    add

---
+++ c_class/c_lesson08_save.c/c_lesson08_save.c/main.c
@@ -0,0 +1,30 @@
+// 은행계좌 구현하기
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// amount가 양수이면 입금이고 음수이면 출금으로 생각한다.
+
+void save(int amount)
+{
+    static long balance = 0;
+    
+    if(amount >= 0)
+        printf("%d\t\t", amount);
+    else
+        printf("\t%d\t", -amount);
+    
+    balance += amount;
+    printf("%d\n", balance);
+}
+
+int main(){
+    printf("====================================\n");
+    printf("입금\t\t출금\t\t잔고\n");
+    printf("====================================\n");
+    save(10000);
+    save(50000);
+    save(-10000);
+    save(30000);
+    printf("====================================\n");
+    return 0;
+}
